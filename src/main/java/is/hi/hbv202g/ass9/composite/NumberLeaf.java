b8eb359ce5d7e65c2eca734265c6d45a1e8c8003
Benedikt Chmielewski <bc222dz@student.lnu.se>
2023-03-17 10:19:12 +0100

    add constructor to NumberLeaf

--- src/main/java/is/hi/hbv202g/ass9/composite/NumberLeaf.java
+++ src/main/java/is/hi/hbv202g/ass9/composite/NumberLeaf.java
@@ -3,6 +3,10 @@ package is.hi.hbv202g.ass9.composite;
 public class NumberLeaf implements MathExpression {
     private int number;
 
+    public NumberLeaf(int number) {
+        this.number = number;
+    }
+
     public int getResult() {
         return number;
     }
