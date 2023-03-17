b3e9646c301b598988a47dde385d6de4481f193e
Benedikt Chmielewski <bc222dz@student.lnu.se>
2023-03-17 10:14:35 +0100

    update variable-names in PlusComposite

--- src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
+++ src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
@@ -3,22 +3,22 @@ package is.hi.hbv202g.ass9.composite;
 import java.util.List;
 import java.util.ArrayList;
 public class PlusComposite implements MathExpression{
-    private List<MathExpression> leafs=new ArrayList<MathExpression>();
+    private List<MathExpression> children =new ArrayList<MathExpression>();
     public int getResult() {
         int runningTotal = 0;
-        for (MathExpression child : leafs) {
+        for (MathExpression child : children) {
             runningTotal += child.getResult();
         }
         return runningTotal;
     }
     public void add(MathExpression leaf) {
-        leafs.add(leaf);
+        children.add(leaf);
     }
     public void remove(MathExpression leaf) {
-        leafs.remove(leaf);
+        children.remove(leaf);
     }
-    public List<MathExpression> getLeaf() {
-        return leafs;
+    public List<MathExpression> getChildren() {
+        return children;
     }
 }
 
