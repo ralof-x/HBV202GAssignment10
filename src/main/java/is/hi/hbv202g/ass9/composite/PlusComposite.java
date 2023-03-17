f399eaf6c2565d53c574e282135ea68ae887df3e
Benedikt Chmielewski <bc222dz@student.lnu.se>
2023-03-17 10:11:46 +0100

    Add methods and bodies to PlusComposite

--- src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
+++ src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
@@ -1,7 +1,25 @@
 package is.hi.hbv202g.ass9.composite;
 
-public class PlusComposite {
-    public static void main(String[] args) {
-
+import java.util.List;
+import java.util.ArrayList;
+public class PlusComposite implements MathExpression{
+    private List<MathExpression> leafs=new ArrayList<MathExpression>();
+    public int getResult() {
+        int runningTotal = 0;
+        for (MathExpression child : leafs) {
+            runningTotal += child.getResult();
+        }
+        return runningTotal;
+    }
+    public void add(MathExpression leaf) {
+        leafs.add(leaf);
+    }
+    public void remove(MathExpression leaf) {
+        leafs.remove(leaf);
+    }
+    public List<MathExpression> getLeaf() {
+        return leafs;
     }
 }
+
+
